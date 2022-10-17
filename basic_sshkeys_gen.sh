4354882a7c1694e841fbafa85d4777f1ba3041cc
Kali <kali@kali>
2022-10-17 12:19:27 -0400

    Push #2

--- basic_sshkeys_gen.sh
+++ basic_sshkeys_gen.sh
@@ -5,24 +5,29 @@ file="$HOME/.ssh/authorized_keys"
 
 if [ ! -d "$directory" ]; then 
 	echo "Directory does not exist, making it..."
-	mkdir $HOME/.ssh;
+	mkdir $HOME/.ssh
 else
-	echo "Directory exists.";
+	echo "Directory exists."
 fi
 
 if [ ! -f "$file" ]; then
 	echo ".ssh/authorized_keys file does not exist, making it..."
-	touch $HOME/.ssh/authorized_keys;
+	touch $HOME/.ssh/authorized_keys
 else
-	echo "File exists.";
+	echo "File exists."
 fi
 
 sudo chmod 770 $HOME/.ssh/authorized_keys
 
-echo "Creating public-private key pair in $HOME/.ssh folder named as $USER."
-ssh-keygen -t rsa -N '' -f $HOME/.ssh/$USER
-
-cat $HOME/.ssh/$USER.pub >> $HOME/.ssh/authorized_keys
+if [ $1 ]; then 
+	echo "Creatiing public-private key pair in $HOME/.ssh folder named as $1."
+	ssh-keygen -t rsa -N '' -f $HOME/.ssh/$1
+	cat $HOME/.ssh/$1.pub >> $HOME/.ssh/authorized_keys
+else	
+	echo "Creating public-private key pair in $HOME/.ssh folder named as $USER."
+	ssh-keygen -t rsa -N '' -f $HOME/.ssh/$USER
+	cat $HOME/.ssh/$USER.pub >> $HOME/.ssh/authorized_keys
+fi
 
 sudo chmod 600 $HOME/.ssh/$USER
 sudo chmod 600 $HOME/.ssh/authorized_keys
